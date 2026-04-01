e1e2a550817f756f443734168860d0400d39d9ac
AshishRaushan12 <ashishraushan23@gmail.com>
2026-04-01 10:02:23 -0700

    Add files via upload
    practice program in java

---
+++ Basics/Data type/Program.java
@@ -0,0 +1,72 @@
+class Program
+{
+	public static void main(String... pro)
+	{
+		byte a=(byte)3000; byte b=(byte)400; int ab;
+		ab=a+b;
+		System.out.println("Byte to Byte in int result");
+		System.out.println(ab);
+		
+		short c=7675; short d= 98; int e;
+		e= c+d;
+		System.out.println("Short to short in int result");
+		System.out.println(e);
+		
+		char ch=545; char ch1=345; int f;
+		f=ch+ch1;
+		System.out.println("Char to Char in int Result");
+		System.out.println(f);
+		
+		byte g=100; short h=30; int i;
+		i=g+h;
+		System.out.println("Byte to Short in int result");
+		System.out.println(i);
+		
+		int j=65547;long k=657676787858l;  long u;
+		u=j+k;
+		System.out.println("int to long in long result");
+		System.out.println(u);
+		 
+		int m=234; long n=1234l; long o;
+		o=m+n;
+		System.out.println("int to long in int result");
+		System.out.println(o);
+
+		boolean aw= true;
+		System.out.println(aw);
+		
+		char ch7='a';
+		System.out.println(ch7);
+		int w=20;
+		int d1=2;
+		int c2=w/d1;
+		System.out.println(c2);
+		
+		char ch5='a';
+		int ascii=ch5;
+		System.out.println(ch5);
+		System.out.println(ascii);
+		
+		
+		byte a1=22;
+		short b2=a;
+		System.out.println(a1);
+		System.out.println(b2);
+
+		
+		
+	}
+}
+		
+		 
+		 
+		 
+
+		
+
+		
+
+		
+		
+		
+	
---
+++ Basics/Data type/Program2.java
@@ -0,0 +1,83 @@
+class Program
+{
+	public static void main(String...args)
+	{
+		int h=3; int w=4; int a;
+		a=h*w;
+		System.out.println(a);
+	
+		byte aaa=(byte)3000;
+		System.out.println(aaa);
+		
+		double na=6.74667377d;
+		System.out.println(na);
+		int va=45;
+		int vb=12;
+		int gc=va+vb;
+		System.out.println(gc);
+		
+		float ma=3.876f;
+		System.out.println(ma);
+		
+		long sa=7223372076543l;
+		System.out.println(sa);
+		
+		int nta=2;
+		int ntb=12;
+ 		int ntc=nta*ntb;
+ 		System.out.println(ntc);
+ 		
+ 		int la,lb,lc;
+		la=23;
+		lb=12;
+		lc=la+lb;
+		System.out.println(lc);
+		
+		short oa=32766;
+		System.out.println(oa);
+		
+		int ta=2; int tb=23; int sub=ta-tb;
+ 		System.out.println(sub);
+ 		
+ 		int ja =20;int jb=30;
+		ja=ja+jb;
+		jb=ja-jb;
+		ja=ja-jb;
+		System.out.println(ja);
+		System.out.println(jb);
+		
+		double fa=4.98222d;int fb=4665;double div;
+		div=fb/fa;
+		System.out.println(div);
+		
+		
+		byte qa=(byte)3000; byte qb=(byte)4000; int sum;
+		sum=qa+qb;
+		System.out.println(sum);
+		
+		byte vat=124; byte vbt=100;int vct;
+		vct=vat+vbt;
+		System.out.println(vct);
+		
+		double ra=4.98222d;float rb=4.665f;double sum1;
+		sum1=rb+ra;
+		System.out.println(sum1);
+ 		
+ 		int xa=34;int xv=45; int xc;
+		xc=xa+xv;
+		System.out.println(xc);
+		
+		short ia=32766; short ib=32766; int ic;
+		ic=ia+ib;
+		System.out.println(ic);
+		
+		int za=23;int zb=33; int zsum;
+		System.out.print("enter two number=");
+		zsum=za+zb;
+		System.out.println(zsum);
+		
+
+	
+	
+	}
+}
